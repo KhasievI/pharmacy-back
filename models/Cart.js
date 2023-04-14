c30cdd70aec512c90233f9fec666b3d746ae7747
Islam <khasievislamcloud@gmail.com>
2023-04-14 10:45:34 +0300

    ..

--- models/Cart.js
+++ models/Cart.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-  cart: [{
-    pharmacy: { type: String, required: true },
-    img: String,
-    medId: { type: mongoose.Schema.Types.ObjectId, ref: "Medicine" },
-    count: Number,
-    price: Number,
-  }],
-  customer: [{
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    city: { type: String, required: true },
-    street: { type: String, required: true },
-    houseNumber: { type: String, required: true },
-    comment: String
-  }]
-})
+  cart: [
+    {
+      pharmacy: { type: String, required: true },
+      img: String,
+      medId: { type: mongoose.Schema.Types.ObjectId, ref: "Medicine" },
+      count: Number,
+      price: Number,
+    },
+  ],
+  customer: [
+    {
+      name: { type: String, required: true },
+      phone: { type: String, required: true },
+      city: { type: String, required: true },
+      street: { type: String, required: true },
+      houseNumber: { type: String, required: true },
+      comment: String,
+    },
+  ],
+});
 
 export default mongoose.model("Cart", cartSchema);
-
